9320a6099eb3577be592b5f2739da5fd74aee325
Jonathan Ceresa <jceresa@techargonauts.com>
2018-09-09 18:22:24 -0400

    implementation of the app from the stub files.

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, AVAudioPlayerDelegate{
     
+    var audioPlayer : AVAudioPlayer!
+    let soundArray : [String] = ["note1", "note2", "note3", "note4", "note5", "note6", "note7", ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,11 +22,21 @@ class ViewController: UIViewController{
 
     @IBAction func notePressed(_ sender: UIButton) {
         
-        
+        playSound(tag : soundArray[sender.tag - 1])
         
     }
     
-  
+    func playSound(tag : String) -> Void {
+        let soundURL = Bundle.main.url(forResource: tag, withExtension: "wav")
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+        } catch {
+            print(error)
+        }
+        
+        audioPlayer.play()
+    }
 
 }
 
